1cdd9eaafc23a2cf400f06de77c12abf8967fb84
Carolina Chavez <scchavezd@gmail.com>
2024-08-20 18:52:16 -0300

    Cambio metodo de actualizacion de estado

--- publication/Publication.js
+++ publication/Publication.js
@@ -1,27 +1,27 @@
 class Publication {
-    constructor(title, attachedFile, leadAuthor) {
-      this.title = title;
-      this.attachedFile = attachedFile;
-      this.authors = []
-      this.leadAuthor = leadAuthor;
-      this.state = 'draft';
-      this.sendDate = new Date();
-    }
-      changeState(newState) {
-      this.state = newState;
-    }
+  constructor(title, attachedFile, leadAuthor) {
+    this.title = title;
+    this.attachedFile = attachedFile;
+    this.authors = []
+    this.leadAuthor = leadAuthor;
+    this.state = 'draft';
+    this.sendDate = new Date();
+  }
+
+  updateState(newState) {
+    this.state = newState;
+  }
 
   getType() {
     throw new Error("El método 'getType()' debe ser implementado.");
   }
 
 
-
-  addAuthor(user){
+  addAuthor(user) {
     this.authors.push(user)
   }
 
-  getPublicationInfo(){
+  getPublicationInfo() {
     console.log(`Publicacion ${this.title}, estado: ${this.state}, tipo: ${this.getType()}`)
   }
 }
