fd9631dfb68cb51050fff5ac66e691d59a60d3fe
fcorinaldesi <fcorinaldesi@unpaz.edu.ar>
2024-08-20 23:06:21 -0300

    feat: :white_check_mark: add new test

--- test/conference.test.js
+++ test/conference.test.js
@@ -22,8 +22,37 @@ describe('Conference', () => {
     });
 
     test('Deberia devolver informacion de los tracks', () => {
-       // console.log = jest.fn(); // Mock console.log to test output
+        conference.registerUser(chairUser);
+        conference.createTrack('regular', 'Software Engineering', '2024-12-31', 'best', chairUser);
+        conference.createTrack('workshop', 'AI Innovations', '2024-11-30', 'best', chairUser);
+        conference.createTrack('poster', 'Big Data', '2024-10-31', 'best', chairUser);
 
-      conference.getTracksInfo()
+       const trackInfo = conference.getTracksInfo();
+
+       expect(trackInfo).toHaveLength(3);
+       expect(trackInfo[0]).toContain('Software Engineering');
+       expect(trackInfo[1]).toContain('AI Innovations');
+       expect(trackInfo[2]).toContain('Big Data');
+       expect(trackInfo[0]).toContain('Tipo: regular');
+       expect(trackInfo[1]).toContain('Tipo: workshop');
+       expect(trackInfo[2]).toContain('Tipo: poster');
+
+    });
+
+    test('Un track regular debe aceptar solo artículos regulares', () => {
+        const regularTrack =  TestDataFactory.createRegularTrack()
+        const posterArticle =  TestDataFactory.createPosterPublicationExample1()
+
+        expect(() => {
+            regularTrack.submitArticle(posterArticle);
+        }).toThrow('El tipo de la publicación es incorrecto. Esperado: regular, Enviado: poster');
+    });
+
+    test('Un track poster debe aceptar solo artículos poster', () => {
+        const posterTrack =  TestDataFactory.createPosterTrack()
+        const regularArticle = TestDataFactory.createRegularPublicationExample1()
+        expect(() => {
+            posterTrack.submitArticle(regularArticle);
+        }).toThrow('El tipo de la publicación es incorrecto. Esperado: poster, Enviado: regular');
     });
 });
