7babfe7d147f8b89f678c9d4de76f23022e09aa6
fernando corinaldesi <you@example.com>
2024-08-31 12:37:22 -0300

    feat: :sparkles: add tests for user

---
+++ test/user.test.js
@@ -0,0 +1,56 @@
+const dataFactory = require('./testDataFactory')
+const Constants = require('../utils/Constants')
+
+describe('User Class', () => {
+    let user;
+
+    beforeEach(() => {
+        user = dataFactory.createUser();
+    });
+
+    test('Deberia agregar y quitar roles correctamente', () => {
+        user.addRole('reviewer');
+        expect(user.getRoles()).toContain('reviewer');
+
+        user.removeRole('reviewer');
+        expect(user.getRoles()).not.toContain('reviewer');
+    });
+
+    test('should check if user has a role', () => {
+        user.addRole('chair');
+        expect(user.hasRole('chair')).toBe(true);
+        expect(user.hasRole('reviewer')).toBe(false);
+    });
+
+    test('Deberia mostrar la informacion del usuario cuando tiene un rol', () => {
+        console.log = jest.fn();
+        user.addRole('author');
+        user.getUserInfo();
+        expect(console.log).toHaveBeenCalledWith('Carolina Chavez tiene los roles: author');
+    });
+
+    test('Deberia mostrar la informacion del usuario cuando no tiene un rol', () => {
+        console.log = jest.fn();
+        user.getUserInfo();
+        expect(console.log).toHaveBeenCalledWith('Carolina Chavez usuario registrado');
+    });
+
+    test('deberia agregar el rol author', () => {
+        const author = dataFactory.createUserFerAuthor();
+        expect(author.hasRole(Constants.USER_ROLE.AUTHOR)).toBe(true);
+    });
+
+    test('deberia agregar el rol chair', () => {
+        const chair = dataFactory.createUserFerChair();
+        chair.organizeConference('Probando');
+        expect(chair.hasRole(Constants.USER_ROLE.CHAIR)).toBe(true);
+    });
+
+    test('deberia agregar el rol revisor', () => {
+        const reviewer = dataFactory.createUserFerReview();
+        reviewer.reviewArticle('Probando');
+        expect(reviewer.hasRole(Constants.USER_ROLE.REVIEWER)).toBe(true);
+    });
+
+
+});
